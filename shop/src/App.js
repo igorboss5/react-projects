94d1a178814f4a24fbaa37796f15c8560f27c592
Igor <igor132143@gmail.com>
2023-09-26 19:40:32 +0300

    initialize shop project

---
+++ shop/src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Крісло',
+          img: 'chair-grey.jpeg',
+          desc: 'Lorem ioas asdsa dasds asdsd',
+          category: 'chairs',
+          price: '49.99'
+        },
+        {
+          id: 2,
+          title: 'Стіл',
+          img: 'table.webp',
+          desc: 'Lorem ioas asdsa dasds asdsd',
+          category: 'tables',
+          price: '149.99'
+        },
+        {
+          id: 3,
+          title: 'Диван',
+          img: 'sofa.jpeg',
+          desc: 'Lorem ioas asdsa dasds asdsd',
+          category: 'chairs',
+          price: '229.99'
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items}/>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default App;
