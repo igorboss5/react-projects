ac62b9482abf4b59e34513fdd296822aaebe8eef
Igor <igor132143@gmail.com>
2023-09-28 20:19:36 +0300

    implement functionality for adding products to the cart

--- shop/src/components/Header.js
+++ shop/src/components/Header.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaShoppingCart } from "react-icons/fa";
+import Order from './Order';
 
-export default function Header() {
+const showOrders = (props) => {
+    return (
+        <div>
+            {props.orders.map(el => (
+                <Order key={el.id} item={el} />
+            ))}
+        </div>
+    )
+}
 
+const showNothing = () => {
+    return (
+        <div className='empty'>
+            <h2>Пусто</h2>
+        </div>
+    )
+}
+
+export default function Header(props) {
+    let [cartOpen, setCartOpen] = useState(false)
     return (
         <header>
             <div>
@@ -11,6 +31,16 @@ export default function Header() {
                     <li>Контакти</li>
                     <li>Профіль</li>
                 </ul>
+                <FaShoppingCart
+                    onClick={() => setCartOpen(cartOpen = !cartOpen)} 
+                    className={`shop-cart-button ${cartOpen && 'active'}`}
+                />
+                {cartOpen && (
+                    <div className='shop-cart'>
+                        {props.orders.length > 0 ?
+                            showOrders(props) : showNothing()}
+                    </div>
+                )}
             </div>
             <div className='presentation'></div>
         </header>
