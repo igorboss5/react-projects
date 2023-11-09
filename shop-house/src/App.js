3358a18b19f305a0c97d10d7b03ed6adfb9c3416
Igor <igor132143@gmail.com>
2023-11-09 20:33:11 +0200

    update app

--- shop-house/src/App.js
+++ shop-house/src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Items from "./components/Items";
-import Categories from "./components/Categories";
-import ShowFullItem from "./components/ShowFullItem";
 
 class App extends React.Component {
   constructor(props) {
@@ -36,55 +32,15 @@ class App extends React.Component {
           category: 'chairs',
           price: '229.99'
         }
-      ],
-      showFullItem: false,
-      fullItem: {}
-
+      ]
     }
-    this.state.currentItems = this.state.items
-    this.addToOrder = this.addToOrder.bind(this)
-    this.deleteOrder = this.deleteOrder.bind(this)
-    this.chooseCatagory = this.chooseCategory.bind(this)
-    this.onShowItem = this.onShowItem.bind(this)
   }
   render() {
     return (
       <div className="wrapper">
-        <Header orders={this.state.orders} onDelete={this.deleteOrder} />
-        <Categories chooseCatagory={this.chooseCategory} />
-        <Items onShowItem={this.onShowItem} items={this.state.currentItems} onAdd={this.addToOrder}/>
-        {this.state.showFullItem && <ShowFullItem onShowItem={this.onShowItem} onAdd={this.addToOrder} item={this.state.fullItem} />}
-        <Footer />
+        <Header />
       </div>
     )
-  }
-
-  onShowItem(item) {
-    this.setState({fullItem: item})
-    this.setState({showFullItem: !this.state.showFullItem})
-  }
-
-  chooseCategory(category) {
-    if(category === 'all') {
-      this.setState({currentItems: this.state.items})
-      return
-    }
-    this.setState({
-      currentItems: this.state.items.filter(el => el.category === category)
-    })
-  }
-
-  deleteOrder(id) {
-    this.setState({orders: this.state.orders.filter(el => el.id !== id)})
-  }
-
-  addToOrder(item) {
-    let isInArray = false
-    this.state.orders.forEach(el => {
-      if(el.id === item.id) isInArray = true
-    })
-    if(!isInArray)
-      this.setState({orders: [...this.state.orders, item]})
   }
 }
 
--- shop-house/src/components/Header.js
+++ shop-house/src/components/Header.js
@@ -1,53 +1,17 @@
-import React, { useState } from 'react'
+import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
-import Order from './Order';
-
-const showOrders = (props) => {
-    let sum = 0;
-    props.orders.forEach(el => {
-        sum += Number.parseFloat(el.price)
-    })
-    return (
-        <div>
-            {props.orders.map(el => (
-                <Order onDelete={props.onDelete} key={el.id} item={el} />
-            ))}
-            <p className='sum'>Сума: {new Intl.NumberFormat().format(sum)}$</p>
-        </div>
-    )
-}
-
-const showNothing = () => {
-    return (
-        <div className='empty'>
-            <h2>Пусто</h2>
-        </div>
-    )
-}
 
 export default function Header(props) {
-    let [cartOpen, setCartOpen] = useState(false)
-    return (
+
+    return(
         <header>
-            <div>
-                <span className='logo'>House Staff</span>
-                <ul className='nav'>
-                    <li>Про нас</li>
-                    <li>Контакти</li>
-                    <li>Профіль</li>
-                </ul>
-                <FaShoppingCart
-                    onClick={() => setCartOpen(cartOpen = !cartOpen)} 
-                    className={`shop-cart-button ${cartOpen && 'active'}`}
-                />
-                {cartOpen && (
-                    <div className='shop-cart'>
-                        {props.orders.length > 0 ?
-                            showOrders(props) : showNothing()}
-                    </div>
-                )}
-            </div>
-            <div className='presentation'></div>
+            <span className="logo">House Staff</span>
+            <ul className="nav">
+                <li>Про нас</li>
+                <li>Контакти</li>
+                <li>Профіль</li>
+            </ul>
+            <FaShoppingCart className="shop-cart-button" />
         </header>
     )
 }
